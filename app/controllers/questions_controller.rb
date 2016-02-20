94519d6bc206e7488b4e99ffa14e360ae75ca88e
Reedlee <reedlee3@yandex.ru>
2016-02-20 16:11:10 +0600

    Добавил редактирование Вопроса

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -24,9 +24,19 @@ class QuestionsController < ApplicationController
   end
 
   def edit
+    @test = Test.find(params[:test_id])
+    @question = Question.find(params[:id])
   end
 
   def update
+    @test = Test.find(params[:test_id])
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+      flash[:notice] = "Вопрос успешно отредактирован!"
+      redirect_to test_path(@test)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
