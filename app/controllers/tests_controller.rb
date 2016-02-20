1191d20a8a3c2fed8cde3985038334cff1211878
Reedlee <reedlee3@yandex.ru>
2016-02-20 14:12:04 +0600

    Создал контролер create для post запроса, нужный для создания test

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -11,6 +11,13 @@ class TestsController < ApplicationController
   end
 
   def create
+    @test = Test.new(test_params)
+    if @test.save
+      flash[:success] = 'Тест успешно создан!'
+      redirect_to tests_path
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -20,5 +27,9 @@ class TestsController < ApplicationController
   end
 
   def destroy
+  end
+  private
+  def test_params
+    params.require(:test).permit( :name, :description)
   end
 end
