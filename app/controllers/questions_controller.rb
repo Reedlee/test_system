49f4688b9e7f7b59bbc3f2d21e5ca4b5118e2278
Reedlee <reedlee3@yandex.ru>
2016-02-20 14:47:44 +0600

    Добавил создание вопроса

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,9 +6,19 @@ class QuestionsController < ApplicationController
   end
 
   def new
+    @test = Test.find(params[:test_id])
+    @question = @test.questions.build
   end
 
   def create
+    @test = Test.find(params[:test_id])
+    @question = @test.questions.build(question_params)
+    if @question.save
+      flash[:notice] = "Вопрос успешно создан!"
+      redirect_to test_path(@test)
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -18,5 +28,9 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
+  end
+  private
+  def question_params
+    params.require(:question).permit(:text, :test_id)
   end
 end
