45d15992858cce75495f961f3c2bf6abbd15e9f2
Reedlee <reedlee3@yandex.ru>
2016-02-20 17:06:17 +0600

    Добавил проверку на правильный ответ перед тем как отобразить форму ответа

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,15 @@
 class Question < ActiveRecord::Base
   belongs_to :test
   has_many :answers
+
+  def has_correct_answer_before?
+    answers = self.answers
+    answers.each do |answer|
+      if answer.correct
+        return true
+      end
+    end
+    return false
+  end
+
 end
