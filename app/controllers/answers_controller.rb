7a1089731e559fd72743cc1a315f3ed8d727ddf7
Reedlee <reedlee3@yandex.ru>
2016-02-20 16:18:45 +0600

    Добавил редактирование Answers Добавил css для notice Удалил лишние вьюшки

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -16,7 +16,7 @@ class AnswersController < ApplicationController
     @question = Question.find(params[:question_id])
     @answer = @question.answers.build(answer_params)
     if @answer.save
-      flash[:notice] = "Ответ успешно создан!"
+      flash[:success] = "Ответ успешно создан!"
       redirect_to test_question_path(@test, @question)
     else
       render 'new'
@@ -24,9 +24,21 @@ class AnswersController < ApplicationController
   end
 
   def edit
+    @test = Test.find(params[:test_id])
+    @question = Question.find(params[:question_id])
+    @answer = Answer.find(params[:id])
   end
 
   def update
+    @test = Test.find(params[:test_id])
+    @question = Question.find(params[:question_id])
+    @answer = Answer.find(params[:id])
+    if @answer.update(answer_params)
+      flash[:notice] = "Ответ успешно отредактирован!"
+      redirect_to test_question_path(@test, @question)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
