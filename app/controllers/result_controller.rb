e9710bc733675bb748eb75196651efe79356ebb5
Reedlee <reedlee3@yandex.ru>
2016-02-20 19:50:37 +0600

    Создал методы для отображения и приема формы

--- app/controllers/result_controller.rb
+++ app/controllers/result_controller.rb
@@ -1,2 +1,26 @@
 class ResultController < ApplicationController
+  def test_form
+    @test = Test.find(params[:id])
+    @questions = @test.questions
+
+  end
+  def create_form
+
+    @answers = test_params
+    @a = params[:question]
+
+    render :text => "#{test_params[:report]}".html_safe
+  end
+  def create_report
+
+  end
+  private
+  def test_params
+    params.require(:result).permit( :test,:st_name, :st_surname, :st_group)
+  end
+
+  def report_params
+
+  end
+
 end
