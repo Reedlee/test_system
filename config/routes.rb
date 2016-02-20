e8cffd953c6a103734b3d20879433ecdd30203fc
Reedlee <reedlee3@yandex.ru>
2016-02-20 14:35:08 +0600

    Добавил nested model в путях

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  resources :tests, :questions, :answers, :test_results
+  root 'tests#index'
+  resources :tests do
+    resources :questions do
+      resources :answers
+    end
+  end
+  resources :test_results
 end
